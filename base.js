8fc2404b34c66b6f1d8fd54f92e8bee59f46ea9b
Wesley Epperly <wepperly@nrao.edu>
2026-09-22 16:56:23 -0400

    modified:   base.js

--- base.js
+++ base.js
@@ -1,63 +1,6 @@
 // reference location for acronym files for the entire extension
 // organize alphabetically by 'name'
 const fileReference = [
-/*  {
-    ref: "af",
-  	  name: "Air Force",
-    default: false,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/af.json",
-  },
-  {
-    ref: "agile",
-    name: "Agile Terms",
-    default: true,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/agile.json",
-  },
-  {
-    ref: "cms",
-    name: "CMS Portfolio",
-    default: false,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/cms.json",
-  },
-  {
-    ref: "default",
-    name: "Fearless",
-    default: true,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/fearless.json",
-  },
-  {
-    ref: "nara",
-    name: "National Archives and Records Administration",
-    default: false,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/nara.json",
-  },
-  {
-    ref: "nmaahc",
-    name: "National Museum of African American History and Culture",
-    default: false,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/nmaahc.json",
-  },
-  {
-    ref: "software",
-    name: "Software",
-    default: true,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/software.json",
-  },
-  {
-    ref: "phrase",
-    name: "Terms and Phrases",
-    default: false,
-    url:
-      "https://raw.githubusercontent.com/FearlessSolutions/acronym_plugin/master/acronyms/phrase.json",
-  },
-*/ 
   {
     ref: "nrao",
     name: "NRAO",
